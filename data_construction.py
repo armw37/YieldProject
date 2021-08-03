eac9150bfbc966b12c75a6f3d5b3090f15273b27
armw37 <anderson.rm.white@gmail.com>
2021-08-03 01:36:05 -0400

    Data is ready, use everything up to Lasso normal to load the data You are welcome to start a different file if you'd like all the lasso stuff is probably buggy as I havent ran up to there yet

--- data_construction.py
+++ data_construction.py
@@ -5,62 +5,183 @@ from pathlib import PurePath
 import numpy as np
 import datetime as dt
 from pandas.tseries.holiday import USFederalHolidayCalendar
+from scipy.interpolate import interp1d
+from sklearn.svm import SVR
 
 
 #========================================================================#
-# Data import                                                            #
+# interpolation functions                                                #
 #========================================================================#
 
+def det_interp(x, kind='linear'):
+    """ A helper function for deterministic time seres interpolation
 
-root = PurePath() 
-raw_data = root / 'raw_data'
+    Args
+    ----
+    x -- a dummy variable for the dataframe's columns
 
-# files
-economics_file = 'economics.csv'
-yields_file = 'FED-SVENY.csv'
+    kwargs
+    ------
+    kind -- one of scipy.interpolate.inter1d kwargs
 
+    return
+    ------
+    interpolated values of the whole time series
 
-# import data
-economics = pd.read_csv(raw_data / economics_file)
-yields = pd.read_csv(raw_data / yields_file)
+    """
 
+    index = pd.Series(np.arange(x.shape[0]), index=x.index)
 
-#========================================================================#
-# clean data                                                             #
-#========================================================================#
+    notnull = pd.notnull(x)
 
-economics.index = pd.to_datetime(economics['sasdate'], format="%m/%d/%Y")
-economics = economics.iloc[:,1:] # drop date column
+    t = index[notnull]
+    
+    y = x[notnull]
 
-# nan strategy is to drop as of now
-economics = economics[~(economics.apply(np.isnan)).apply(any, axis=1)]
-economics = economics.iloc[:-9,:] # done by inspection
+    f = interp1d(t.values, y.values, kind=kind)
 
-
-yields.index = pd.to_datetime(yields['Date'], format="%Y-%m-%d")
-yield_col = ['SVENY01',	'SVENY02', 'SVENY03', 'SVENY05', 'SVENY07', 
-        'SVENY10', 'SVENY20', 'SVENY30']
-yields = yields[yield_col]
+    return pd.Series(f(index), index=x.index, name=x.name)
 
 
-#========================================================================#
-# Join data by date                                                      #
-#========================================================================#
+def ml_interp(x, model, **model_kwargs):
+    """ A helper function for ML time seres interpolation
 
-# Right now we will move econ dates forward to next trading date
+    Args
+    ----
+    x -- a dummy variable for the dataframe's columns
+    model -- a scikit learn model class
+    model_kwargs -- tuple of kwargs to pass to the model constructor
 
-bday_us = pd.offsets.CustomBusinessDay(calendar=USFederalHolidayCalendar())
+    
+    return
+    ------
+    interpolated values of the whole time series
 
-economics.index = economics.index +  bday_us
+    """
 
-# 04/01/1999 gets moved when to 04/02/1999 it shouldn't
-as_list = economics.index.tolist()
-idx = as_list.index(dt.datetime(1999, 4, 2))
-as_list[idx] = dt.datetime(1999, 4, 1)
-economics.index = as_list
+    index = pd.Series(np.arange(x.shape[0]), index=x.index)
 
-full = pd.concat([economics, yields], axis=1, join="inner")
+    notnull = pd.notnull(x)
+
+    t = index[notnull].values.reshape(-1, 1)
+    
+    y = x[notnull]
+    
+    regr = model(**model_kwargs)
+
+    regr.fit(t, y)
+
+    yhat = regr.predict(index.values.reshape(-1, 1))
+
+    return pd.Series(yhat, index=x.index, name=x.name)
 
 
 
+def main():
 
+    #=====================================================================#
+    # Data import                                                         #
+    #=====================================================================#
+
+
+    root = PurePath() 
+    raw_data = root / 'raw_data'
+    sentiment = root / 'sentiment_analysis'
+
+    # files
+    economics_file = 'economics.csv'
+    yields_file = 'FED-SVENY.csv'
+    sentiment_file = root / 'daily_sentiment_score.csv'
+
+    # import data
+    economics = pd.read_csv(raw_data / economics_file)
+    yields = pd.read_csv(raw_data / yields_file)
+    sent = pd.read_csv(sentiment / sentiment_file)
+
+
+    #=====================================================================#
+    # clean data                                                          #
+    #=====================================================================#
+
+    economics.index = pd.to_datetime(economics['sasdate'], 
+            format="%m/%d/%Y")
+    economics = economics.iloc[:,1:] # drop date column
+
+    # nan strategy is to drop as of now
+    economics = economics[~(economics.apply(np.isnan)).apply(any, axis=1)]
+    economics = economics.iloc[:-9,:] # done by inspection
+
+
+    yields.index = pd.to_datetime(yields['Date'], format="%Y-%m-%d")
+    yield_col = ['SVENY10']
+
+    yields = yields[yield_col]
+
+    sent.index = pd.to_datetime(sent['date'], format="%Y-%m-%d")
+
+    sent = sent.drop('date', axis=1)
+
+    #=====================================================================#
+    # Join data by date                                                   #
+    #=====================================================================#
+
+    # Right now we will move econ dates forward to next trading date
+    bday_us = pd.offsets.CustomBusinessDay(
+            calendar=USFederalHolidayCalendar())
+
+    economics.index = economics.index +  bday_us
+
+    # 04/01/1999 gets moved when to 04/02/1999 it shouldn't
+    as_list = economics.index.tolist()
+    idx = as_list.index(dt.datetime(1999, 4, 2))
+    as_list[idx] = dt.datetime(1999, 4, 1)
+    economics.index = as_list
+
+    full = pd.concat([economics, yields], axis=1, join="outer")
+
+    # Join the tweets
+    full = pd.concat([full, sent], axis=1, join="outer")
+
+    #========================================================================#
+    # reclean                                                                #
+    #========================================================================#
+
+    # drop everything before first appearance of rpi
+
+    inds = full[pd.notnull(full['RPI'])].index.values
+    full = full.iloc[full.index.get_loc(inds[0]):,:]
+
+    # drop everything after the last rpi
+    full = full.iloc[:full.index.get_loc(inds[-1])+1,:]
+
+    # drop weird generated column 
+    full = full.drop('Unnamed: 0', axis=1)
+
+    # reorder columns 
+    cols = full.columns.tolist()
+    cols = cols[:-2] + cols[-2:][::-1]
+    full = full[cols]
+
+    # drop na's added by sentiment score
+    full = full[full['SVENY10'].notna()]
+
+    # replace null sentiment score with 0.5
+    full['probability'].fillna(0.5, inplace=True)
+
+    # save the file in the data folder
+    full.to_csv(raw_data / 'cleaned_full_data.csv')
+
+    return 0
+
+
+#econ_interp = full.iloc[:,:-1].apply(det_interp, axis=0)
+#full = pd.concat([econ_interp, full.iloc[:,-1]], axis=1, join="outer")
+
+#test = ml_interp(full['RPI'], SVR, kernel='rbf', epsilon=0.1)
+
+#econ_ml = full.iloc[:,:-1].apply(ml_interp, axis=0, 
+#        args=(SVR,), kernel='rbf', epsilon=0.1)
+#full = pd.concat([econ_ml, full.iloc[:,-1]], axis=1, join="outer")
+
+if __name__ == '__main__':
+    main()
